1ff89ee757bae60bc59a68853a7773371990e656
Buzz Tilford <tilford20@gmail.com>
2018-07-12 18:08:55 -0700

    refactor technology function into object file

--- js/technology.js
+++ js/technology.js
@@ -11,7 +11,7 @@ function Technology(description,
 	this.has_been_researched = has_been_researched;
 	this.items = [];
 	this.cost = {fiber: fiber_cost, metal: metal_cost, gelatin: gelatin_cost};
-	
+
     this.prerequisites = prerequisites;
 
     this.operation = operation;
@@ -21,11 +21,25 @@ function Technology(description,
 	if(this.cost.gelatin_cost === undefined){
 	  this.cost = {fiber: 0, metal: 0, gelatin: 0};
 	}
-	
-}
 
-Technology.prototype.unlock = function(technology) {
+};
+
+Technology.prototype.unlock = function(technology){
 	technology.researched = true;
+   }
+
+
+function create_technology_html(technology, technology_list_html){
+   for (i in technology){
+       if (technology[i].has_been_researched){
+           technology_list_html += ("<p>" + technology[i].description + "</p>");
+       }
+   }
+   technology_list_html += "<hr>";
+   for (i in technology){
+       if (!technology[i].has_been_researched && technology[i].can_be_researched){
+           technology_list_html += ("<p>" + technology[i].description + "</p>");
+       }
+   }
+   return technology_list_html;
 }
-
-
