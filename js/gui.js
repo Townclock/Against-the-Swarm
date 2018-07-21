bf2773076469a2a80f6f53eb2761a32f373ba37c
Buzz Tilford <tilford20@gmail.com>
2018-07-21 11:10:03 -0700

    merged most recent dev

--- js/gui.js
+++ js/gui.js
@@ -45,20 +45,6 @@ function update_ui( monster){
 
 }
 
-var current_knight_frame = 1;
-function update_knight_animation(){
-    var current_knight_sprite = "-";
-    var knight_scale = 350;
-    if (current_knight_frame >= 19) {
-        current_knight_frame = 1;
-    }
-    else {
-        current_knight_frame += 1;
-    }
-    for (i in game.fighters)
-        document.getElementsByClassName("knight_sprite")[i].style.backgroundPosition = current_knight_sprite + knight_scale * current_knight_frame + 'px 0%';
-}
-
 var current_fire_frame = 1;
 function update_fire_animation(){
     var current_fire_sprite = "-";
@@ -72,4 +58,39 @@ function update_fire_animation(){
    for (i in game.towns){
         document.getElementsByClassName("fire_sprite")[i].style.backgroundPosition = current_fire_sprite + fire_scale * current_fire_frame + 'px 0%';
    }
+}
+
+var knight_current_frame = 1;
+var spider_current_frame = 1;
+function update_animation(){
+    update_knight_animation();
+    update_spider_animation();
+}
+
+function update_knight_animation(){
+    var current_sprite = "-";
+    var scale = 350;
+    if (knight_current_frame >= 19) {
+        knight_current_frame = 1;
+    }
+    else {
+        knight_current_frame += 1;
+    }
+    for (i = 0; i < document.getElementsByClassName("knight_sprite").length; i++) {
+        document.getElementsByClassName("knight_sprite")[i].style.backgroundPosition = current_sprite + scale * knight_current_frame + 'px 0%';
+    }
+}
+
+function update_spider_animation(){
+    var current_sprite = "-";
+    var scale = 350;
+    if (spider_current_frame >= 24) {
+        spider_current_frame = 1;
+    }
+    else {
+        spider_current_frame += 1;
+    }
+    for (i = 0; i < document.getElementsByClassName("spider_sprite").length; i++) {
+        document.getElementsByClassName("spider_sprite")[i].style.backgroundPosition = current_sprite + scale * spider_current_frame + 'px 0%';
+    }
 }
